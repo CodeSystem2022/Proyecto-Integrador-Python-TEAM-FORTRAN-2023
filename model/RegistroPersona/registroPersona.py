09a5c7e5ca999d631c95bebfd1b4618583551488
califerdan <califerdan@gmail.com>
2023-06-25 21:47:20 -0300

    Funcionalidad botón mostrar base de datos #20

--- model/RegistroPersona/registroPersona.py
+++ model/RegistroPersona/registroPersona.py
@@ -13,10 +13,14 @@ class iniciar:
         self.ventana.bt_actualizar.clicked.connect(self.seccionActualizar)
         self.ventana.bt_eliminar_2.clicked.connect(self.seccionEliminar)
         
+        # apenas ingresa a la opcion registroPersona carga y muestra la base de datos
+        mostrarEmpleados(self)
+        
         app.exec()
     # funcionalidades de botones que llevan a diferentes secciones
     def seccionMostrarBaseDatos(self):
         self.ventana.stackedWidget.setCurrentIndex(0)
+        mostrarEmpleados(self)
         
     def seccionRegistrar(self):
         self.ventana.stackedWidget.setCurrentIndex(1)
