3ab0917554e24858ce3d3879c097d974a5444258
califerdan <califerdan@gmail.com>
2023-06-25 22:17:07 -0300

    Función insertar empleado #18

--- model/RegistroPersona/solicitudesBD.py
+++ model/RegistroPersona/solicitudesBD.py
@@ -7,3 +7,16 @@ datosBD = {
     "host" : "",
     "port" : ""
 }
+
+def insertarEmpleado(empleado):
+        conn = psycopg2.connect(**datosBD)
+        cursor = conn.cursor()
+        query = '''INSERT INTO empleados (nombre, apellido, dni, cuit, categoria, sueldo) VALUES (%s, %s, %s, %s, %s, %s)'''
+        try:
+            datos = (empleado.nombre, empleado.apellido, empleado.dni,
+                       empleado.cuit, empleado.categoria, empleado.sueldo)
+            cursor.execute(query, (datos))
+        except Exception as e:
+            print(e)
+        conn.commit()
+        cursor.close()
