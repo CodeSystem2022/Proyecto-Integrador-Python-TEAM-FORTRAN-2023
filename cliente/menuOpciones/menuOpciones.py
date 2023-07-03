9cac52416cc5dd0d42018d299b44ea6df78e097c
Gabriel Hidalgo <gabriel.hid.orl@gmail.com>
2023-07-03 16:32:06 -0300

    Solucionando errores #44
    Se arreglaron unos errores que impedía levantar el menú opciones general de la aplicación.

--- cliente/menuOpciones/menuOpciones.py
+++ cliente/menuOpciones/menuOpciones.py
@@ -1,42 +1,31 @@
 import sys
-from PyQt6.QtWidgets import QApplication,QWidget
-from menuOpciones import Ui_from
-class Ui_from(QWidget):
-    def __init__(self,form):
-        super(Ui_from, self).__init__()
-        self.ui=Ui_from
+from PyQt6.QtWidgets import QApplication, QWidget
+from menuOpcionesGUI import Ui_Form
+
+class Ui_iniciar(QWidget):
+    def __init__(self):
+        super(Ui_iniciar, self).__init__()
+        self.ui = Ui_Form()
         self.ui.setupUi(self)
+
+        # Botones
+        self.ui.bt_registrar.clicked.connect(self.registrar)
+        self.ui.bt_categoria.clicked.connect(self.categoria)
+        self.ui.bt_estadistica.clicked.connect(self.estadistica)
+
         self.show()
-       #Botones
-        self.ui.registrar.clicked.connect(self.registrar)
-        self.ui.categoria.clicked.connect(self.categoria)
-        self.ui.estadistica.clicked.connect(self.estadistica)
-        #redirreccionar
-        self.setWindowTitle("RegistroPersonal")
-        self.setWindowTitle("Categoria")
-        self.setWindowTitle("Estadistica")
+
+    def registrar(self):
+        print("registrar")
+
+    def categoria(self):
+        print("categoria")
+
+    def estadistica(self):
+        print("estadistica")
 
 
-        def registrar(self):
-            print("registrar")
-        def categoria(self):
-            print("categoria")
-        def estadistica():
-            print("estadistica")
-
-
-
-if __name__=="__main__":
-    app=QApplication(sys.argv)
-    menuOpciones=Ui_from()
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ui_iniciar = Ui_iniciar()
     sys.exit(app.exec())
-
-
-
-
-
-
-
-
-
-
