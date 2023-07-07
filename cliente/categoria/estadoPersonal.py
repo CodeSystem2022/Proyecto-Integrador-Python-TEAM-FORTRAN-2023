447aecd244f67017d53f75dfa0cd687abcb4c7b6
Gabriel Hidalgo <88288730+HidoGG@users.noreply.github.com>
2023-07-07 02:29:20 -0300

    Gabriel h (#67)
    * Actualización registroPersona
    
    Había problemas para ver GUI, se arreglo.
    
    * Actualizando GUI#39
    
    Se actualizo, la parte de diseño de persona la parte GUI.
    
    * Arreglando GUI de registro de persona #39
    
    Se arreglo el GUI de registro de persona, se eliminaron botones, y se agrego uno para volver a menu.
    
    * Solucionando errores #44
    
    Se arreglaron unos errores que impedía levantar el menú opciones general de la aplicación.
    
    * Delete workspace.xml
    
    * Delete .gitignore
    
    * Delete .gitignore
    
    * Delete Proyecto-Integrador-Python-TEAM-FORTRAN-2023.iml
    
    * Delete profiles_settings.xml
    
    * Delete misc.xml
    
    * Delete modules.xml
    
    * Delete vcs.xml
    
    * Conexión entre menús #46
    
    Se agrego la conexión entre menú general y registro persona. Tambien se estuvo arreglando unos errores de la parte de login, y se mejoro la parte grafica.
    
    * Se actualizo Estado Personal
    
    Se agrego funcionalidad al archivo estado Personal, y tambien datos de conexión a la base de datos.
    
    * Se agrego el import os
    
    Se agreo el import os al archivo de diseñoEstadistica.py
    
    * Agrego importación en menuOpciones
    
    Se subio la improtación de Estadistica en menuOpciones
    
    * Subiendo cambios
    
    * mejorando archivo . Falta

--- cliente/categoria/estadoPersonal.py
+++ cliente/categoria/estadoPersonal.py
@@ -1,4 +1,5 @@
 import sys
+
 import os
 import subprocess
 from PyQt6.QtWidgets import QApplication, QMainWindow, QMessageBox
@@ -19,20 +20,31 @@ class Formulario(QMainWindow, Ui_MainWindowfrom):
 
         # Botones
         self.bt_buscar.clicked.connect(self.search_data)
-        #self.bt_limpiar.clicked.connect(self.clear_data)
-        #self.bt_regresar.clicked.connect(self.return_data)
-        #self.bt_salir.clicked.connect(self.output_data)
+
 
     # Funciones
     def search_data(self):
         dni_a_buscar = self.in_dni.text()
         empleado = EmpleadoDao.buscar_por_dni(dni_a_buscar)
 
-    #def clear_data(self):
-        #self.in_dni.clear()
+        if empleado:
+            print("Empleado encontrado:", empleado)  # Mensaje de depuración
+            # Obtener los datos del empleado
+            dni = empleado.dni
+            nombre = empleado.nombre
+            apellido = empleado.apellido
+            cuit = empleado.cuit
+            categoria = empleado.categoria
+            sueldo = empleado.sueldo
 
-def llamarRecibo():
-    subprocess.run(['python', 'Recibo.py'])
+            # Llamar directamente a la función obtener_recibo en ReciboSueldo
+            print("Llamando a la función obtener_recibo en Recibo.py")  # Mensaje de depuración
+            app = QApplication.instance()  # Obtener la instancia de la aplicación
+            recibo = ReciboSueldo(dni, nombre, apellido, cuit, categoria, sueldo)
+            recibo.show()
+        else:
+            QMessageBox.warning(self, "Error", "Empleado no encontrado.")
+
 
 if __name__ == '__main__':
     llamarRecibo()
