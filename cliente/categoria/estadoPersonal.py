5e052c41c878afe5d8fcbc4819759bf55b55f918
Gabriel Hidalgo <88288730+HidoGG@users.noreply.github.com>
2023-07-06 23:02:00 -0300

    Gabriel h (#66)
    * Actualización registroPersona
    
    Había problemas para ver GUI, se arreglo.
    
    * Actualizando GUI#39
    
    Se actualizo, la parte de diseño de persona la parte GUI.
    
    * Arreglando GUI de registro de persona #39
    
    Se arreglo el GUI de registro de persona, se eliminaron botones, y se agrego uno para volver a menu.
    
    * Solucionando errores #44
    
    Se arreglaron unos errores que impedía levantar el menú opciones general de la aplicación.
    
    * Delete workspace.xml
    
    * Delete .gitignore
    
    * Delete .gitignore
    
    * Delete Proyecto-Integrador-Python-TEAM-FORTRAN-2023.iml
    
    * Delete profiles_settings.xml
    
    * Delete misc.xml
    
    * Delete modules.xml
    
    * Delete vcs.xml
    
    * Conexión entre menús #46
    
    Se agrego la conexión entre menú general y registro persona. Tambien se estuvo arreglando unos errores de la parte de login, y se mejoro la parte grafica.
    
    * Se actualizo Estado Personal
    
    Se agrego funcionalidad al archivo estado Personal, y tambien datos de conexión a la base de datos.
    
    * Se agrego el import os
    
    Se agreo el import os al archivo de diseñoEstadistica.py
    
    * Agrego importación en menuOpciones
    
    Se subio la improtación de Estadistica en menuOpciones
    
    * Subiendo cambios

--- cliente/categoria/estadoPersonal.py
+++ cliente/categoria/estadoPersonal.py
@@ -1,58 +1,42 @@
 import sys
 import os
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
-
-
+import subprocess
 from PyQt6.QtWidgets import QApplication, QMainWindow, QMessageBox
 from formulario import Ui_MainWindowfrom
 from model.empleado_Dao import EmpleadoDao
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 
 
 
 class Formulario(QMainWindow, Ui_MainWindowfrom):
     def __init__(self):
-        super(Formulario, self).__init__()
+        super().__init__()
         try:
             self.setupUi(self)
         except Exception as e:
             print("Error al cargar el archivo UI:", str(e))
 
-        #Botones
+        # Botones
         self.bt_buscar.clicked.connect(self.search_data)
-        self.bt_limpiar.clicked.connect(self.clear_data)
-        self.bt_regresar.clicked.connect(self.return_data)
-        self.bt_salir.clicked.connect(self.output_data)
+        #self.bt_limpiar.clicked.connect(self.clear_data)
+        #self.bt_regresar.clicked.connect(self.return_data)
+        #self.bt_salir.clicked.connect(self.output_data)
 
     # Funciones
     def search_data(self):
         dni_a_buscar = self.in_dni.text()
         empleado = EmpleadoDao.buscar_por_dni(dni_a_buscar)
 
-
-
+    #def clear_data(self):
+        #self.in_dni.clear()
 
 def llamarRecibo():
     subprocess.run(['python', 'Recibo.py'])
 
-
-if __name__ == '__name__':
-
-    llamarRecibo()
-
-    def clear_data(self):
-        self.in_dni.clear()
-
-
 if __name__ == '__main__':
+    llamarRecibo()
     app = QApplication([])
     window = Formulario()
     window.show()
-    app.exec()
-
-
-
-
-
-
-
+    sys.exit(app.exec())
