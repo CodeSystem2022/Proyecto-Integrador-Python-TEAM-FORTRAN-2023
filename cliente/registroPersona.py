6e67195c8a6ac34af1c0feba9eb9dd3c463982b9
Gabriel Hidalgo <gabriel.hid.orl@gmail.com>
2023-07-07 14:40:41 -0300

    subiendo cambios
    se agrego. cambios

--- cliente/registroPersona.py
+++ cliente/registroPersona.py
@@ -1,9 +1,10 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
-from solicitudesBD import mostrarEmpleados, insertarEmpleado, buscarEmpleado, actualizarEmpleado, eliminarEmpleado
-from database.empleado import Empleado
+from PyQt6.QtCore import Qt
 from diseñoRegistroPersonaPy import Ui_MainWindow
 import menuOpciones
+from model.empleado_Dao import EmpleadoDao, crear_tabla, Empleado
+
 
 class Iniciar(QMainWindow, Ui_MainWindow):
     def __init__(self):
@@ -12,9 +13,9 @@ class Iniciar(QMainWindow, Ui_MainWindow):
             self.setupUi(self)  # Reemplaza loadUi por setupUi
         except Exception as e:
             print("Error al cargar el archivo UI:", str(e))
+
         # Agregar la variable "ventana" como un atributo de la instancia actual
         self.ventana = self
-
 
         self.ventana.bt_datos.clicked.connect(self.seccionMostrarBaseDatos)
         self.ventana.bt_registrar.clicked.connect(self.seccionRegistrar)
@@ -29,14 +30,14 @@ class Iniciar(QMainWindow, Ui_MainWindow):
         self.ventana.bt_buscar_borrar.clicked.connect(self.buscarEliminarEmpleado)
         self.ventana.bt_eliminar.clicked.connect(self.botonEliminarEmpleado)
 
-        mostrarEmpleados(self)
+        self.seccionMostrarBaseDatos()
 
         self.show()
 
-
     def seccionMostrarBaseDatos(self):
         self.ventana.stackedWidget.setCurrentIndex(0)
-        mostrarEmpleados(self)
+        empleados = EmpleadoDao.seleccionar()
+        self.mostrarEmpleados(empleados)
 
     def seccionRegistrar(self):
         self.ventana.stackedWidget.setCurrentIndex(1)
@@ -52,87 +53,113 @@ class Iniciar(QMainWindow, Ui_MainWindow):
         self.menu_principal = menuOpciones.Ui_iniciar()
         self.menu_principal.show()
 
+    def mostrarEmpleados(self, empleados):
+        self.ventana.tabla_productos.setRowCount(len(empleados))
+        self.ventana.tabla_productos.setColumnCount(6)
+        for i, empleado in enumerate(empleados):
+            self.ventana.tabla_productos.setItem(i, 0, QTableWidgetItem(empleado.nombre))
+            self.ventana.tabla_productos.setItem(i, 1, QTableWidgetItem(empleado.apellido))
+            self.ventana.tabla_productos.setItem(i, 2, QTableWidgetItem(empleado.dni))
+            self.ventana.tabla_productos.setItem(i, 3, QTableWidgetItem(empleado.cuit))
+            self.ventana.tabla_productos.setItem(i, 4, QTableWidgetItem(empleado.categoria))
+            self.ventana.tabla_productos.setItem(i, 5, QTableWidgetItem(str(empleado.sueldo)))
+
     def agregarEmpleado(self):
-        nombre = self.ventana.reg_nombre.text()
-        apellido = self.ventana.reg_apellido.text()
-        dni = self.ventana.reg_dni.text()
-        cuit = self.ventana.reg_cuit.text()
-        categoria = self.ventana.reg_categoria.text()
-        sueldo = self.ventana.reg_sueldo.text()
+        try:
+            nombre = self.ventana.reg_nombre.text()
+            apellido = self.ventana.reg_apellido.text()
+            dni = int(self.ventana.reg_dni.text())
+            cuit = self.ventana.reg_cuit.text()
+            categoria = self.ventana.reg_categoria.text()
+            sueldo = int(self.ventana.reg_sueldo.text())
 
-        datosEmpleado = Empleado(1, nombre, apellido, dni, cuit, categoria, sueldo)
-        insertarEmpleado(datosEmpleado)
-
-        self.ventana.reg_nombre.setText('')
-        self.ventana.reg_apellido.setText('')
-        self.ventana.reg_dni.setText('')
-        self.ventana.reg_cuit.setText('')
-        self.ventana.reg_categoria.setText('')
-        self.ventana.reg_sueldo.setText('')
+            empleado = Empleado(nombre, apellido, dni, cuit, categoria, sueldo)  # Crear instancia de Empleado
+            EmpleadoDao.insertar(empleado)  # Pasar la instancia de Empleado como argumento
+            self.ventana.reg_nombre.clear()
+            self.ventana.reg_apellido.clear()
+            self.ventana.reg_dni.clear()
+            self.ventana.reg_cuit.clear()
+            self.ventana.reg_categoria.clear()
+            self.ventana.reg_sueldo.clear()
+            self.seccionMostrarBaseDatos()
+        except Exception as e:
+            print("Error al agregar empleado:", str(e))
 
     def actualizarEmpleado(self):
-        nombre = self.ventana.act_nombre.text()
-        apellido = self.ventana.act_apellido.text()
-        dni = self.ventana.act_dni.text()
-        cuit = self.ventana.act_cuit.text()
-        categoria = self.ventana.act_categoria.text()
-        sueldo = self.ventana.act_sueldo.text()
+        try:
+            nombre = self.ventana.act_nombre.text()
+            apellido = self.ventana.act_apellido.text()
+            dni = int(self.ventana.act_dni.text())
+            cuit = self.ventana.act_cuit.text()
+            categoria = self.ventana.act_categoria.text()
+            sueldo = float(self.ventana.act_sueldo.text().replace(',', '.'))
 
-        datosEmpleado = Empleado(1, nombre, apellido, dni, cuit, categoria, sueldo)
-        actualizarEmpleado(datosEmpleado)
+            empleado = Empleado(nombre, apellido, dni, cuit, categoria, sueldo)
+            EmpleadoDao.actualizar(empleado)
 
-        self.ventana.act_nombre.setText('')
-        self.ventana.act_apellido.setText('')
-        self.ventana.act_dni.setText('')
-        self.ventana.act_cuit.setText('')
-        self.ventana.act_categoria.setText('')
-        self.ventana.act_sueldo.setText('')
+            self.ventana.act_nombre.clear()
+            self.ventana.act_apellido.clear()
+            self.ventana.act_dni.clear()
+            self.ventana.act_cuit.clear()
+            self.ventana.act_categoria.clear()
+            self.ventana.act_sueldo.clear()
+
+            self.seccionMostrarBaseDatos()
+        except Exception as e:
+            print("Error al actualizar empleado:", str(e))
 
     def buscarActualizarEmpleado(self):
-        dniBuscado = self.ventana.act_buscar.text()
-        dniEncontrado = buscarEmpleado(dniBuscado)
-
-        self.ventana.act_nombre.setText(dniEncontrado[0])
-        self.ventana.act_apellido.setText(dniEncontrado[1])
-        self.ventana.act_dni.setText(str(dniEncontrado[2]))
-        self.ventana.act_cuit.setText(str(dniEncontrado[3]))
-        self.ventana.act_categoria.setText(dniEncontrado[4])
-        self.ventana.act_sueldo.setText(str(dniEncontrado[5]))
+        try:
+            dniBuscado = self.ventana.act_buscar.text()
+            print("Valor de dniBuscado:", dniBuscado)  # Agrega esta línea de depuración
+            empleado = EmpleadoDao.buscar_por_dni(dniBuscado)
+            if empleado:
+                self.ventana.act_nombre.setText(empleado.nombre)
+                self.ventana.act_apellido.setText(empleado.apellido)
+                self.ventana.act_dni.setText(empleado.dni)
+                self.ventana.act_cuit.setText(empleado.cuit)
+                self.ventana.act_categoria.setText(empleado.categoria)
+                self.ventana.act_sueldo.setText(str(empleado.sueldo))  # Convertir a cadena de texto
+        except Exception as e:
+            print("Error al actualizar empleado:", str(e))
 
     def buscarEliminarEmpleado(self):
-        dniBuscado = self.ventana.eliminar_buscar.text()
-        dniEncontrado = buscarEmpleado(dniBuscado)
-
-        tabla = self.ventana.tabla_eliminar
-        tabla.setRowCount(1)
-        tabla.setColumnCount(6)
-        tabla.setItem(0, 0, QTableWidgetItem(dniEncontrado[0]))
-        tabla.setItem(0, 1, QTableWidgetItem(dniEncontrado[1]))
-        tabla.setItem(0, 2, QTableWidgetItem(str(dniEncontrado[2])))
-        tabla.setItem(0, 3, QTableWidgetItem(str(dniEncontrado[3])))
-        tabla.setItem(0, 4, QTableWidgetItem(dniEncontrado[4]))
-        tabla.setItem(0, 5, QTableWidgetItem(str(dniEncontrado[5])))
+        try:
+            dniBuscado = self.ventana.eliminar_buscar.text()
+            empleado = EmpleadoDao.buscar_por_dni(dniBuscado)
+            if empleado:
+                tabla = self.ventana.tabla_eliminar
+                tabla.setRowCount(1)
+                tabla.setColumnCount(6)
+                tabla.setItem(0, 0, QTableWidgetItem(empleado.nombre))
+                tabla.setItem(0, 1, QTableWidgetItem(empleado.apellido))
+                tabla.setItem(0, 2, QTableWidgetItem(empleado.dni))
+                tabla.setItem(0, 3, QTableWidgetItem(empleado.cuit))
+                tabla.setItem(0, 4, QTableWidgetItem(empleado.categoria))
+                tabla.setItem(0, 5, QTableWidgetItem(str(empleado.sueldo)))
+        except Exception as e:
+         print("Error al eliminar1 empleado:", str(e))
 
     def botonEliminarEmpleado(self):
-        tabla = self.ventana.tabla_eliminar
+        try:
+            tabla = self.ventana.tabla_eliminar
 
-        nombre = tabla.item(0, 0).text()
-        apellido = tabla.item(0, 1).text()
-        dni = tabla.item(0, 2).text()
-        cuit = tabla.item(0, 3).text()
-        categoria = tabla.item(0, 4).text()
-        sueldo = tabla.item(0, 5).text()
+            dni = tabla.item(0, 2).text()
+            empleado = EmpleadoDao.buscar_por_dni(dni)
 
-        datosEmpleado = Empleado(3, nombre, apellido, dni, cuit, categoria, sueldo)
-        eliminarEmpleado(datosEmpleado)
+            if empleado:
+                EmpleadoDao.eliminar(empleado)
+                tabla.setItem(0, 0, QTableWidgetItem(''))
+                tabla.setItem(0, 1, QTableWidgetItem(''))
+                tabla.setItem(0, 2, QTableWidgetItem(''))
+                tabla.setItem(0, 3, QTableWidgetItem(''))
+                tabla.setItem(0, 4, QTableWidgetItem(''))
+                tabla.setItem(0, 5, QTableWidgetItem(''))
+                self.ventana.eliminar_buscar.setText('')
+                self.seccionMostrarBaseDatos()
+        except Exception as e:
+         print("Error al eliminar2 empleado:", str(e))
 
-        tabla.setItem(0, 0, QTableWidgetItem(''))
-        tabla.setItem(0, 1, QTableWidgetItem(''))
-        tabla.setItem(0, 2, QTableWidgetItem(''))
-        tabla.setItem(0, 3, QTableWidgetItem(''))
-        tabla.setItem(0, 4, QTableWidgetItem(''))
-        tabla.setItem(0, 5, QTableWidgetItem(''))
-        self.ventana.eliminar_buscar.setText('')
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
