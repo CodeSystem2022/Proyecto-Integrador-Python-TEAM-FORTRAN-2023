13ffbbba4ddb1ca4d63fdf934e996cd3159b7c38
Gabriel Hidalgo <gabriel.hid.orl@gmail.com>
2023-07-07 02:21:45 -0300

    mejorando archivo . Falta

--- cliente/categoria/Recibo.py
+++ cliente/categoria/Recibo.py
@@ -1,12 +1,12 @@
 import sys
-#from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QLineEdit, QPushButton
-from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QLineEdit, QPushButton
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QPushButton
 from model.logger_base import log
 from model.conexion import Conexion
 from model.empleado_Dao import EmpleadoDao
 
+
 class ReciboSueldo(QWidget):
-    def __init__(self):
+    def __init__(self, dni=None, nombre=None, apellido=None, cuit=None, categoria=None, sueldo=None):
         super().__init__()
         self.setWindowTitle("Recibo de Sueldo")
         self.setGeometry(100, 100, 600, 400)
@@ -27,15 +27,6 @@ class ReciboSueldo(QWidget):
         self.label_total_deduc = QLabel()
         self.label_total_neto = QLabel()
 
-        # Mostrar el mensaje al usuario
-        # self.label_mensaje = QLabel("Ingrese el número de DNI del empleado:")
-        # self.layout.addWidget(self.label_mensaje)
-
-        # Ingreso del número de DNI
-        # self.input_dni = QLineEdit()
-        # self.layout.addWidget(self.input_dni)
-
-        # Botón para obtener el recibo de sueldo
         self.btn_obtener_recibo = QPushButton("Obtener Recibo")
         self.btn_obtener_recibo.clicked.connect(self.obtener_recibo)
         self.layout.addWidget(self.btn_obtener_recibo)
@@ -50,52 +41,46 @@ class ReciboSueldo(QWidget):
         self.layout.addWidget(self.label_total_deduc)
         self.layout.addWidget(self.label_total_neto)
 
-        self.setStyleSheet("background-color: white")  # color fondo
+        self.setStyleSheet("background-color: white")
         self.btn_obtener_recibo.setStyleSheet(
-            "background-color: darkblue; color: white;")  # color boton, color fondo y texto
+            "background-color: darkblue; color: white;")
+
+        self.dni = dni
+        self.nombre = nombre
+        self.apellido = apellido
+        self.cuit = cuit
+        self.categoria = categoria
+        self.sueldo = sueldo
 
     def obtener_recibo(self):
-        # Conexión a la base de datos
-        conn = Conexion.obtenerConexion()
-        cursor = conn.cursor()
+        log.debug("Iniciando generación del recibo de sueldo")
+        if self.sueldo is None:
+            log.error("El sueldo no está definido")
+            return  # Salir del método o mostrar un mensaje de error al usuario
 
-        # Obtener el número de DNI ingresado por el usuario
-        numero_dni = self.input_dni.text()
-
-        # Consulta para obtener los datos del empleado
-        cursor.execute("SELECT * FROM Personal WHERE dni = %s", (numero_dni,))
-        empleado = cursor.fetchone()
-
-        # Cerrar conexión
-        cursor.close()
-        Conexion.liberarConexion(conn)
-
-        if empleado:
-            nombre, apellido, dni, cuit, categoria, sueldo = empleado
-
-            datos_empresa = f"""
+        try:
+            datos_empresa = """
                DATOS DE LA EMPRESA
              |---------------------------------------------------|---------------------------------------------------|
                NOMBRE DE LA EMPRESA: Team Fortran               Cuit: 9-52634875-0
              |---------------------------------------------------|---------------------------------------------------|
                DIRECCION: Gotan 985
              |---------------------------------------------------|---------------------------------------------------|
-              """
+            """
 
-            # Datos del empleado
             datos_empleado = f"""
               DATOS DE LOS EMPLEADOS
             |----------------------------------------------------|---------------------------------------------------|
-              APELLIDO: {apellido}                                  NOMBRE :  {nombre}
+              APELLIDO: {self.apellido}                                  NOMBRE :  {self.nombre}
             |----------------------------------------------------|---------------------------------------------------|
-              DNI: {dni}                                            CUIL: {cuit}
+              DNI: {self.dni}                                            CUIL: {self.cuit}
             |----------------------------------------------------|---------------------------------------------------|
-              CATEGORIA: {categoria}                                SUELDO BASICO: ${sueldo}
+              CATEGORIA: {self.categoria}                                SUELDO BASICO: ${self.sueldo}
             |----------------------------------------------------|---------------------------------------------------|
             """
 
-            # Conceptos y montos
-            sueldo_bruto = float(sueldo)
+            sueldo_bruto = float(self.sueldo)
+            log.debug(f"Sueldo bruto: {sueldo_bruto}")
             antiguedad = sueldo_bruto * 5 / 100
             presentismo = sueldo_bruto * 20 / 100
             jubilacion = sueldo_bruto * 11 / 100
@@ -106,44 +91,51 @@ class ReciboSueldo(QWidget):
             neto_a_cobrar = sub_total - total_deducciones
 
             conceptos = f"""
-              CONCEPTOS                            REMUNERATIVO                                         DEDUCCIONES                   
+            CONCEPTOS                            REMUNERATIVO                                         DEDUCCIONES                   
             |--------------------------------|------------------------------------------------|----------------------------------------------|
-              SUELDO BASICO                            ${sueldo}                                                                                
+            SUELDO BASICO                            ${self.sueldo}                                                                                
             |--------------------------------|------------------------------------------------|                                                                
-              ANTIGUEDAD                                ${antiguedad}                                                                            
+            ANTIGUEDAD                                ${antiguedad}                                                                            
             |--------------------------------|------------------------------------------------|                                                                  
-              PRESENTISMO                               ${presentismo}                                                                           
+            PRESENTISMO                               ${presentismo}                                                                           
             |--------------------------------|------------------------------------------------|----------------------------------------------|                                
-              JUBILACION                                                                                           ${jubilacion} 
+            JUBILACION                                                                                           ${jubilacion} 
             |--------------------------------|------------------------------------------------|----------------------------------------------|                                               
-              OBRA SOCIAL                                                                                        ${obra_social}                               
+            OBRA SOCIAL                                                                                        ${obra_social}                               
             |--------------------------------|------------------------------------------------|----------------------------------------------|                        
-              LEY 19032                                                                                             ${ley19032} 
+            LEY 19032                                                                                             ${ley19032} 
             |--------------------------------|------------------------------------------------|----------------------------------------------|
-                                                      TOTAL REMUNERATIVO: {sub_total}            TOTAL DEDUCCIONES: {total_deducciones} 
+                                                          TOTAL REMUNERATIVO: {sub_total}            TOTAL DEDUCCIONES: {total_deducciones} 
             |--------------------------------|------------------------------------------------------------------------------------------------|
-                                                      TOTAL NETO A COBRAR: {neto_a_cobrar}    
+                                                          TOTAL NETO A COBRAR: {neto_a_cobrar}    
             |--------------------------------|------------------------------------------------------------------------------------------------|                                        
             """
 
-            # Oculta las etiquetas
-            #self.label_mensaje.hide()
-            #self.input_dni.hide()
             self.btn_obtener_recibo.hide()
             self.label_datos_empresa.show()
             self.label_datos_empleado.show()
             self.label_conceptos.show()
 
-            # Cambia estilo a fondo azul claro para mostrar el recibo
             self.setStyleSheet("background-color: lightblue;")
 
-            # Actualiza las etiquetas con los datos obtenidos
             self.label_datos_empresa.setText(datos_empresa)
             self.label_datos_empleado.setText(datos_empleado)
             self.label_conceptos.setText(conceptos)
+            log.debug("Recibo de sueldo generado exitosamente")
+        except Exception as e:
+            log.error("Se produjo un error al generar el recibo de sueldo:", str(e))
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    recibo = ReciboSueldo()
+
+    # Verificar si se proporcionaron argumentos y establecer los valores predeterminados en caso contrario
+    if len(sys.argv) >= 7:
+        log.debug("Argumentos proporcionados. Creando instancia de ReciboSueldo con argumentos")
+        recibo = ReciboSueldo(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6])
+    else:
+        log.debug("No se proporcionaron argumentos. Creando instancia de ReciboSueldo sin argumentos")
+        recibo = ReciboSueldo()  # Usar valores predeterminados
+
     recibo.show()
     sys.exit(app.exec())
